4c0e94347b98f777c2474e925596af8588129b0f
kino64 <electricity-technology@hotmail.com>
2016-10-20 08:42:44 +0200

    Add files via upload

---
+++ pInvadersF2-master/lib/ia.php
@@ -0,0 +1,19 @@
+<?php
+class ia
+{
+  public $fila = "1";
+  public $columna = "3";
+
+  // declaraciones
+  public function getColumna(){
+    return $this->fila;
+  }
+  public function getFila(){
+    return $this->columna;
+  }
+
+}
+
+
+
+ ?>
